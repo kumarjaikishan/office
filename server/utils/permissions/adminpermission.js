ceb13f6dad0a48fda53f877912e09e4b8eee83bc
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2025-08-03 18:01:02 +0530

    DFYDF

---
+++ server/utils/permissions/adminpermission.js
@@ -0,0 +1,17 @@
+// config/permissions.js
+const Adminpermissions = {
+  attandence: [1, 2, 3, 4], // 1 = read, 2 =write, 3 =update, 4= delete
+  branch: [1, 2, 3],
+  company: [1],
+  department: [1, 2, 3, 4],
+  employee: [1, 2, 3, 4],
+  enrty: [1, 2, 3, 4],
+  holiday: [1, 2, 3, 4],
+  leave: [1, 2, 3, 4],
+  ledger: [1, 2, 3, 4],
+  notification: [1, 2, 3, 4],
+  salary: [1, 2, 3, 4],
+  user: [1, 2, 3],
+};
+
+module.exports = Adminpermissions;
